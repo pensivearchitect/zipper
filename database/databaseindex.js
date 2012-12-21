c7962ffb4a89eb1371811a2ef604bce439fb095c
Joshua <pensivearchitect@gmail.com>
2012-12-21 13:05:39 -0800

    Deleted cruft/moved stuff to the necessary directories

--- database/databaseindex.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-var sunninja = require("./resources/sunninja.html");
-var onRequest = require("../server/server.js");
-
-fs.read(sunninja, "filetobezipped", 1, 64, null, onRequest);
